5054e7717a220072dff9df04482f8246257eb586
heshan <supunabayarathnabs@gmail.com>
2025-11-17 10:21:31 +0530

    add favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,10 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+        <link rel="icon" type="image/png" href="{{ asset('src/images/logo/favicon.ico') }}">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -17,7 +21,7 @@
     </head>
     <body>
         {{ $slot }}
-        
+
         @livewireScripts
     </body>
     {{-- <body class="font-sans text-gray-900 antialiased">
